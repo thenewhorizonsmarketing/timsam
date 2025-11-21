5d96ff715f95ea1152df653fca1eaf61ece23a26
Timothy Samuel King <sam@thenewhorizonsmarketing.com>
2025-11-21 08:30:05 -0600

    Update vite config with base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/timsam/',  // Change to your repo name
   server: {
     host: "::",
     port: 8080,
